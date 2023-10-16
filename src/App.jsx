5d335d85d5610345e94f14c68d18461bad2d188a
unknown <sazwerling@gmail.com>
2023-10-15 17:56:56 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => (
+  <main className="relative">Nav
+  <section className="xl:padding-1 wide:padding-r padding-b">
+      HERO
+  </section>
+  <section className="padding">Popular Products</section>
+  <section className="padding">Super Quality</section>
+  <section className="padding padding-x py-10">Services</section>
+  <section className="padding">Special Offers</section>
+  <section className="padding bg-pale-blue">Customer Reviews</section>
+  <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+  <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+) 
+
+
+export default App;
