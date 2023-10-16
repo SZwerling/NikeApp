d36b09b1a31cd0d0c1f78a26813e8cbe198464ee
unknown <sazwerling@gmail.com>
2023-10-16 15:05:17 -0700

    hero text, stats, button

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
