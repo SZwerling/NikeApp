5ee230bd0075ac0e12ae01658a451f442851d499
unknown <sazwerling@gmail.com>
2023-10-21 11:36:04 -0700

    customer reviews section and review card

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,30 @@
-
+import { reviews } from '../constants'
+import ReviewCard from '../components/ReviewCard';
 
 const CustomerReviews = () => {
-  return (
-    <div>CustomerReviews</div>
-  )
-}
+   return (
+      <section className="max-container">
+         <h3 className="font-palanquin text-center text-4xl font-bold">
+            What Our<span className="text-coral-red"> Customers</span> Say
+         </h3>
+         <p className="m-auto info-text mt-4 max-w-lg text-center">
+            Hear genuine stories from our satisfied customers about their
+            exceptional experience with us
+         </p>
+         <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+          {reviews.map((review) => (
+            <ReviewCard key={review.customerName} {...review}/>
+          ))}
+         </div>
+      </section>
+   );
+};
 
-export default CustomerReviews
+export default CustomerReviews;
+
+// {
+//   imgURL: customer1,
+//   customerName: 'Morich Brown',
+//   rating: 4.5,
+//   feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+// },
