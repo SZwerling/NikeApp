4a0800cd16e3e256bd878b497df06b60bcd21e14
unknown <sazwerling@gmail.com>
2023-10-20 20:32:31 -0700

    special offers, update button to accept more props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+   label,
+   iconURL,
+   backgroundColor,
+   borderColor,
+   textColor,
+}) => {
    return (
-      <button className="flex justify-center items-center gap-2 px-7 py-4 border-coral-red font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+      <button
+         className={`flex justify-center items-center gap-2 px-7 py-4 rounded-full border font-montserrat text-lg leading-none
+         ${
+            backgroundColor
+               ? `${backgroundColor} ${borderColor} ${textColor}`
+               : "border-coral-red bg-coral-red text-white"
+         }`}
+      >
          {label}
          {iconURL && (
             <img
